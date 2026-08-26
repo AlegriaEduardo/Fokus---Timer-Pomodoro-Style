3b3171065f3b4c846bf2c2db4689d86202680bf9
AlegriaEduardo <alegriaeduardox@gmail.com>
2026-08-26 21:09:04 +0100

    Adicionado comentarios de facil acesso

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const html = document.querySelector('html');
+//Declaração das var dos botões foco, descanso curto, descanso longo
 const focoBt = document.querySelector('.app__card-button--foco') ;
 const curtoBt = document.querySelector('.app__card-button--curto') ;
 const longoBt = document.querySelector('.app__card-button--longo') ;
+//Declaração das var do botão de start/pause
 const startPauseBt = document.querySelector('#start-pause');
 const banner = document.querySelector('.app__image');
+
 const titulo = document.querySelector('.app__title');
 const botoes = document.querySelectorAll('.app__card-button');
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const tempoNaTela = document.querySelector('#timer')
-
+//Músicas utilizadas no projeto, para alterar basta colocar o caminho correto
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
+musica.loop = true // para reprodução em loop
 const contadorIniciado = new Audio('/sons/play.wav');
 const contadorPausado = new Audio('/sons/pause.mp3');
 const fimContador = new Audio('/sons/beep.mp3');
+
 const iniciarOuPausarBt = document.querySelector('#start-pause span');
 const iconePlayPause = document.querySelector('.app__card-primary-butto-icon');
 
-let tempoDecorridoEmSegundos = 1500;
+let tempoDecorridoEmSegundos = 1500; // tempo atual
 let intervaloId = null;
 
-musica.loop = true
-
+// método de reprodução da música
 musicaFocoInput.addEventListener('change', ()=>{
     if (musica.paused){
         musica.play();
@@ -29,23 +33,25 @@ musicaFocoInput.addEventListener('change', ()=>{
         musica.pause()
     }
 })
-
+ 
+//timer 
 focoBt.addEventListener('click', ()=>{
-    tempoDecorridoEmSegundos = 1500;
+    tempoDecorridoEmSegundos = 1500; //25 mins
     alterarContexto('foco');
     focoBt.classList.add('active');
 })
 curtoBt.addEventListener('click', ()=>{
-    tempoDecorridoEmSegundos = 300
+    tempoDecorridoEmSegundos = 300 // 5 mins
     alterarContexto('descanso-curto');
     curtoBt.classList.add('active');
 })
 longoBt.addEventListener('click', ()=>{
-    tempoDecorridoEmSegundos = 900;
+    tempoDecorridoEmSegundos = 900; // 15 mins
     alterarContexto('descanso-longo');
     longoBt.classList.add('active');
 })
 
+//método de alterar fundo e imagem
 function alterarContexto(contexto){
     mostrarTempo();
     botoes.forEach(function(contexto){
@@ -75,7 +81,7 @@ function alterarContexto(contexto){
         break;
     }
 }
-
+//Método do Timer
 const contagemRegressiva = () =>{
     if(tempoDecorridoEmSegundos <=0 ){
         fimContador.play()
@@ -92,7 +98,7 @@ const contagemRegressiva = () =>{
     tempoDecorridoEmSegundos -= 1;
     mostrarTempo()
 }
-
+// Pausar-Retomar timer
 startPauseBt.addEventListener("click", iniciarOuPausar);
 function iniciarOuPausar(){
     if(intervaloId){
@@ -114,10 +120,13 @@ function iniciarOuPausar(){
     focoBt.setAttribute('disabled', true)
 } 
 
+//zerar Timer 
 function zerar (){
     clearInterval(intervaloId);
     intervaloId = null;
 }
+
+//Conversão e exibição do tempo na tela
 function mostrarTempo(){
 const tempo = new Date(tempoDecorridoEmSegundos * 1000);
 const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
